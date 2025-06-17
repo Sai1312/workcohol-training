15c6331cb1de237360597c48f33b128c1154e5cf
Sai1312 <saiharsha1320@gmail.com>
2025-06-17 13:08:02 +0530

    second commit

--- day4.py
+++ day4.py
@@ -1,55 +1,52 @@
-# print("function()\n")
+print("function()\n")
 
-# def welcome_txt():
-#     print("Welcome User ")
-#     return "Welcome!"
+def welcome_txt():
+    print("Welcome User ")
+    return "Welcome!"
 
-# # welcome_txt()
-# print(welcome_txt())
+# welcome_txt()
+print(welcome_txt())
 
 
-# def hello_txt(name):
-#     return f"Hello {name}"
+def hello_txt(name):
+    return f"Hello {name}"
     
-# print(hello_txt("Millen"))
-# print(hello_txt("Jack"))
+print(hello_txt("Millen"))
+print(hello_txt("Jack"))
 
-# def greet(name="Guest"):
-#     print("Hello, " + name)
+def greet(name="Guest"):
+    print("Hello, " + name)
 
-# greet()         
-# greet("Avi")  
+greet()         
+greet("Avi")  
 
-# def celsius(c):
-#     f = (c *(9/5) + 32)
-#     print(f)
+def celsius(c):
+    f = (c *(9/5) + 32)
+    print(f)
+
+celsius(32)
+celsius(int(input("Enter celsius: ")))
+
+print("\n using deocator")
+def my_txt(func): #decorator
+    name = input("Enter name :")
+    def wrapper(c):
+        print(f"Hello {name}!")
+        print("Your Fahrenheit is-")
+        func(c)
+        print("Thank You")
+    return wrapper
+    
+@my_txt
+def celsius(c):
+    f = (c *(9/5) + 32)
+    print(f)
 
 # celsius(32)
-# celsius(int(input("Enter celsius: ")))
-
-# print("\n using deocator")
-# def my_txt(func): #decorator
-#     name = input("Enter name :")
-#     def wrapper(c):
-#         print(f"Hello {name}!")
-#         print("Your Fahrenheit is-")
-#         func(c)
-#         print("Thank You")
-#     return wrapper
-    
-# @my_txt
-# def celsius(c):
-#     f = (c *(9/5) + 32)
-#     print(f)
-
-# # celsius(32)
-# celsius(int(input("Enter celsius: ")))
+celsius(int(input("Enter celsius: ")))
 
 print("\nexception handling") 
 
-# num = int(input("Enter number: "))
-# r = 10/num
-# print(f"result is {r}")
 try:
     num = int(input("Enter number: "))
     r = 10/num   
