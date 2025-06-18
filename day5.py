af781d69375615e23408f73448606dbe1f564108
Sai1312 <saiharsha1320@gmail.com>
2025-06-18 13:00:35 +0530

    initail commit

---
+++ day5.py
@@ -0,0 +1,9 @@
+from module.bill import bill_amount
+
+amount = int(input("Enter amount: "))
+print("Bill amount: ", bill_amount(amount))
+
+import pandas as pd
+
+dt = pd.DataFrame(['a', 'b', 'c', 'd'])
+print(dt) 
---
+++ module/bill.py
@@ -0,0 +1,3 @@
+def bill_amount(num):
+    price = num * 1.05 + 25;
+    return price
